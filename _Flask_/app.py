ed2c6764cfe58ab50f17aab3cbe0b6c67da4e4b9
Karla <prattskarla@gmail.com>
2019-08-24 11:41:39 -0400

    _Flask_ app.py

---
+++ _Flask_/app.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hello, world"
+    
