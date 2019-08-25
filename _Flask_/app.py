f47208eb1a61efffae5199c145519d47cafb2db1
Karla <prattskarla@gmail.com>
2019-08-25 19:29:03 -0400

    static/css-js-templates/html-app.py

--- _Flask_/app.py
+++ _Flask_/app.py
@@ -1,18 +1,15 @@
-
 #import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 from flask import render_template
-
 
 app = Flask(__name__)
 
 @app.route('/')
-def Earthquakes():
-    name='Earthquakes'
-    return render_template('file.html', name=name)
+def project2():
+    title="earthquake"
+    return render_template('file.html', title=title)
     
