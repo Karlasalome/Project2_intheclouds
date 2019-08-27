8daaecb8235c3224bb954bf53fb52cff84db0e53
Hobbs <mhabib5@hotmail.com>
2019-08-27 18:54:23 -0400

    organization of files

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 from flask import render_template
+import static
 
 #################################################
 # Database Setup
